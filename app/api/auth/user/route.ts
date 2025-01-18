40882128dc82c2c136d1e757fedf82af2dad1abc
Josué Rubio <josu_mr@hotmail.com>
2025-01-18 17:41:32 +0100

    going back

--- app/api/auth/user/route.ts
+++ app/api/auth/user/route.ts
@@ -1,11 +1,15 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-  const userCookie = req.cookies.get("user");
-  if (!userCookie) {
+export async function GET(req: Request) {
+  const cookies = req.headers.get("cookie") || "";
+  const userCookie = cookies
+    .split("; ")
+    .find((cookie) => cookie.startsWith("user="));
+  const user = userCookie ? JSON.parse(decodeURIComponent(userCookie.split("=")[1])) : null;
+  console.log("user", user)
+  if (!user) {
     return NextResponse.json({ error: "User not logged in" }, { status: 401 });
   }
 
-  const user = JSON.parse(userCookie.value);
   return NextResponse.json(user);
 }
