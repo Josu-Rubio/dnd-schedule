31753da44a179cab02555dcaded4103a430dc500
Josué Moreira Rubio <josu_mr@hotmail.com>
2025-01-22 20:36:55 +0100

    conditionals on daily route to only send message on monday and ready game everyday

--- app/api/daily/route.ts
+++ app/api/daily/route.ts
@@ -20,6 +20,7 @@ const DaySchema = new mongoose.Schema({
             state: { type: String, enum: ['green', 'yellow'], required: true },
         },
     ],
+    message: { type: Boolean, default: false }, // Added message field
 });
 
 const DayModel = mongoose.models.Day || mongoose.model('Day', DaySchema);
@@ -32,24 +33,31 @@ export async function GET() {
         const startOfNextWeek = format(nextMonday, 'yyyy-MM-dd');
         const endOfNextWeek = format(nextSunday, 'yyyy-MM-dd');
 
+        const roleId = process.env.ROLE_ID; // Use env variable for RoleID
+
         // Format the first and last day of the week in the format dd/MM/yyyy
         const startDate = format(nextMonday, 'dd/MM/yyyy');
         const endDate = format(nextSunday, 'dd/MM/yyyy');
 
-        // Send the "Let's rol this week!" message
-        const embedMonday = {
-            title: "Let's rol this week! 🎲",
-            description: `Votad para ver qué días se pueden jugar la semana que viene.\n\n**Fechas disponibles:**\n${startDate} - ${endDate}\n\nGracias por participar en la planificación de nuestras partidas. ¡Esperamos veros allí!`,
-            url: 'https://dnd-schedule.vercel.app/',
-            color: 0x00ff00, // Green color
-            footer: {
-                text: 'Organizado por Roleros',
-            },
-            timestamp: new Date().toISOString(),
-        };
 
-        const roleId = process.env.ROLE_ID; // Use env variable for RoleID
-        await sendMessageToDiscord(`<@&${roleId}>`, embedMonday);
+        const dayOfWeek = today.getDay();
+        // Check if it's Monday (1), Wednesday (3), or Friday (5)
+        if (dayOfWeek === 1) {
+            // Send the "Let's rol this week!" message
+            const embedMonday = {
+                title: "Let's rol this week! 🎲",
+                description: `Votad para ver qué días se pueden jugar la semana que viene.\n\n**Fechas disponibles:**\n${startDate} - ${endDate}\n\nGracias por participar en la planificación de nuestras partidas. ¡Esperamos veros allí!`,
+                url: 'https://dnd-schedule.vercel.app/',
+                color: 0x00ff00, // Green color
+                footer: {
+                    text: 'Organizado por Roleros',
+                },
+                timestamp: new Date().toISOString(),
+            };
+
+
+            await sendMessageToDiscord(`<@&${roleId}>`, embedMonday);
+        }
 
         // Fetch days from the database to check for eligible days
         console.log(`Checking for votes between ${startOfNextWeek} and ${endOfNextWeek}`);
@@ -62,19 +70,21 @@ export async function GET() {
             const yellowVotes = day.votes.filter((vote: { state: string }) => vote.state === 'yellow').length;
             const totalVotes = greenVotes * 1 + yellowVotes * 0.5;
 
-            return { date: day.date, totalVotes, votes: day.votes };
+            return { date: day.date, totalVotes, votes: day.votes, messageSent: day.message };
         });
 
         // Check for eligible days
-        const eligibleDay = scores.find((score) => score.totalVotes >= 4);
+        const eligibleDay = scores.find((score) => score.totalVotes >= 4 && !score.messageSent);
         if (eligibleDay) {
             console.log(`Eligible day found: ${eligibleDay.date} with ${eligibleDay.totalVotes} total votes`);
 
             const participants = eligibleDay.votes.map((vote: { userId: string; username: string }) => `<@${vote.userId}>`);
 
+            const elegibleFormatDate = format(eligibleDay.date, 'dd/MM/yyyy')
+
             const embedReady = {
                 title: "Tenemos sesión! 🎉",
-                description: `El ${eligibleDay.date} hay personas interesadas en jugar`,
+                description: `El ${elegibleFormatDate} hay personas interesadas en jugar`,
                 fields: [
                     {
                         name: "Participantes Disponibles",
@@ -89,8 +99,15 @@ export async function GET() {
             };
 
             await sendMessageToDiscord(`<@&${roleId}>`, embedReady);
+
+            // Update the message field in the database
+            await DayModel.updateOne(
+                { date: eligibleDay.date },
+                { $set: { message: true } }
+            );
+            console.log(`Message sent and updated for day: ${eligibleDay.date}`);
         } else {
-            console.log('No eligible days found for next week.');
+            console.log('No eligible days found for next week or message already sent.');
         }
 
         return new Response(JSON.stringify({ message: 'Cron job executed successfully' }), {
